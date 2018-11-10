d6ade361dcfee117563143c56880d61aa900c3a0
Dušan Milojković <dmilojkovic76@gmail.com>
2018-11-10 07:53:39 +0100

    struggling with making repeated fetchs with store

---
+++ src/components/ChartComp.vue
@@ -0,0 +1,23 @@
+<script>
+import { Line, mixins } from "vue-chartjs";
+
+import { mapState } from "vuex";
+
+export default {
+  extends: Line,
+  mixins: [mixins.reactiveData],
+  computed: {
+    ...mapState(["BPI_rate", "BPI_time", "chart_Data", "chart_Labels"]),
+  },
+  mutations: {
+  },
+  mounted() {
+    this.renderChart({
+      labels: this.$store.state.chart_Labels,
+      datasets: [{
+        data: this.$store.state.chart_Data,
+      }],
+    });
+  },
+};
+</script>
